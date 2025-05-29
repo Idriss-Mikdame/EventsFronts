19f13313062ae9515987403438ee6839d19d50ec
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-05-29 22:35:41 +0100

    ja fiat la creatio home , about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { AminTemplatComponent } from './amin-templat/amin-templat.component';
+import { AminTemplatComponent } from './navbar/amin-templat.component';
 import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
 import {MatButton, MatButtonModule, MatIconButton} from '@angular/material/button';
 import {MatIcon, MatIconModule} from '@angular/material/icon';
